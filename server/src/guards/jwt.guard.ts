9f03f1ac52b66cb2dad08644588d13b04cd410ec
Michał Rejdych <rejdych.m.r@gmail.com>
2021-02-27 13:59:53 +0100

    Added Injectable decorator to jwt guard.

--- server/src/guards/jwt.guard.ts
+++ server/src/guards/jwt.guard.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+@Injectable()
 class JwtAuthGuard extends AuthGuard('jwt') {}
 
 export default JwtAuthGuard;
